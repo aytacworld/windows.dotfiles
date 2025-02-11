8ad15db2a140224c0cd3428a3c6945c7b4e803cf
Adem Aytac <adem.aytac@ibz.be>
2025-02-11 12:45:23 +0100

    nvim some providers disabled

--- nvim/lua/darkking/set.lua
+++ nvim/lua/darkking/set.lua
@@ -34,3 +34,8 @@ vim.g.mapleader = ' '
 
 -- Disable mouse
 vim.opt.mouse = ''
+
+-- Disable providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
